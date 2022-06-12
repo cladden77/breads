429f738df28e2b0c0c311da6903ae41dc993a3a1
Chris L <chris.ladden@gmail.com>
2022-06-12 11:25:43 -0500

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -16,6 +16,7 @@ function Show ({bread, index}) {
         }
         have gluten.
     </p>
+    <a href={`/breads/${index}/edit`}><button>Edit</button></a>
     <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type="submit" value="DELETE" />
     </form>
