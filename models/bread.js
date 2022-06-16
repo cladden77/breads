87f8b2c9df3c6a51fd4f65517a819173ad5a449a
Chris L <chris.ladden@gmail.com>
2022-06-16 16:33:36 -0500

    Added in a required field and default for our data

--- models/bread.js
+++ models/bread.js
@@ -6,11 +6,12 @@ const { Schema } = mongoose
 
 const breadSchema = new Schema({
   // we will write our schema here
-  name: { type: String },
-  hasGluten: { type: Boolean },
-  image: { type: String }
+  name: { type: String, required: true },
+  hasGluten: Boolean,
+  image: { type: String, default: 'http://placehold.it/500x500.png' }
 })
 
+//Model and export
 // const Bread: The variable we are saving our model to. Conventionally, it should be capitalized and use the singular version of the collection the model is for.
 // mongoose.model: A Mongoose method that creates a model for us based on the arguments we pass it. This is what will later allow us to interact with our Mongo database.
 // Bread: The first argument we passed is the name of the collection we want to connect this model to. As with the variable, this should be capitalized and use the singular version of the collection name. In our case, we want to connect it to a collection named breads, so that becomes Bread when singular and capitalized.
