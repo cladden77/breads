ddbc180873903f1a987fd953861063f1cf7446c6
Chris L <chris.ladden@gmail.com>
2022-06-26 10:18:21 -0500

    refactored code for query optimization

--- views/index.jsx
+++ views/index.jsx
@@ -9,8 +9,8 @@ function Index ({breads, bakers, title}) {
             <ul>
                 {
                     bakers.map((baker) => {
-                        return (<li key={baker.id}>
-                          <a href={`/bakers/${baker.id}`}>{baker.name}</a>  
+                        return (<li key={baker._id}>
+                          <a href={`/bakers/${baker._id}`}>{baker.name}</a>  
                         </li>)
                     })
                 }
@@ -20,7 +20,7 @@ function Index ({breads, bakers, title}) {
                 {
                     breads.map((bread, index) => {
                         return (<li key={index}>
-                          <a href={`/breads/${bread.id}`}>{bread.name}</a>  
+                          <a href={`/breads/${bread._id}`}>{bread.name}</a>  
                         </li>)
                     })
                 }
